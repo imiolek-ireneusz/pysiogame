8406d474e2059f62537a31c34858da951a940869
Oliver@vanderbuerie.de <babyface@78>
2016-09-12 09:39:36 -0700

    added some more line in snowwhite

--- i18n/custom/kbrd/de_cinderella.py
+++ i18n/custom/kbrd/de_cinderella.py
@@ -12,25 +12,24 @@ cinderella = [#1
 "Da war sie zufrieden, denn sie wußte, daß der Spiegel die Wahrheit sagte. Schneewittchen aber wuchs heran und wurde immer schöner, und als es sieben Jahre alt war, war es so schön, wie der klare Tag und schöner als die Königin selbst. Als diese einmal ihren Spiegel fragte: " 
 ]],#2
 [[1,1,1,1,1],
-["In the evening when she had worked till she was weary she had no bed to go to, but had to sleep by the fireside in the ashes. And as on that account she always looked dusty and dirty, they called her Cinderella. ",
-"It happened that the father was once going to the fair, and he asked his two step-daughters what he should bring back for them. ",
-'"Beautiful dresses," said one, "Pearls and jewels," said the second. "And thou, Cinderella," said he, "what wilt thou have?" "Father, break off for me the first branch which knocks against your hat on your way home." ',
-"So he bought beautiful dresses, pearls and jewels for his two stepdaughters, and on his way home, as he was riding through a green thicket, a hazel twig brushed against him and knocked off his hat. ",
-"Then he broke off the branch and took it with him. When he reached home he gave his step-daughters the things which they had wished for, and to Cinderella he gave the branch from the hazel-bush."
+["'Spieglein, Spieglein an der Wand, Wer ist die Schönste im ganzen Land?' so antwortete er: 'Frau Königin, Ihr seid die Schönste hier, Aber Schneewittchen ist tausendmal schöner als Ihr.' Da erschrak die Königin und ward gelb und grün vor Neid. ",
+"Von Stund an, wenn sie Schneewittchen erblickte, kehrte sich ihr das Herz im Leibe herum, so haßte sie das Mädchen. ",
+'Und der Neid und Hochmut wuchsen wie ein Unkraut in ihrem Herzen immer höher, daß sie Tag und Nacht keine Ruhe mehr hatte. Da rief sie einen Jäger und sprach: "Bring das Kind hinaus in den Wald, ich will\'s nicht mehr vor meinen Augen sehen. Du sollst es töten und mir Lunge und Leber zum Wahrzeichen mitbringen." ',
+"Der Jäger gehorchte und führte es hinaus, und als er den Hirschfänger gezogen hatte und Schneewittchens unschuldiges Herz durchbohren wollte, fing es an zu weinen und sprach: 'Ach, lieber Jäger, laß mir mein Leben! Ich will in den wilden Wald laufen und nimmermehr wieder heimkommen.' ",
+"Und weil es gar so schön war, hatte der Jäger Mitleiden und sprach: 'So lauf hin, du armes Kind!' Die wilden Tiere werden dich bald gefressen haben, dachte er, und doch war's ihm, als wäre ein Stein von seinem Herzen gewälzt, weil er es nicht zu töten brauchte. "
 ]],#3
 [[1,1,1,1,1],
-["Cinderella thanked him, went to her mother's grave and planted the branch on it, and wept so much that the tears fell down on it and watered it. ",
-"And it grew, however, and became a handsome tree. Thrice a day Cinderella went and sat beneath it, and wept and prayed, and a little white bird always came on the tree, and if Cinderella expressed a wish, the bird threw down to her what she had wished for. ",
-"It happened, however, that the King appointed a festival which was to last three days, and to which all the beautiful young girls in the country were invited, in order that his son might choose himself a bride. ",
-'When the two step-sisters heard that they too were to appear among the number, they were delighted, called Cinderella and said, "Comb our hair for us, brush our shoes and fasten our buckles, for we are going to the festival at the King\'s palace." ', #"
-'Cinderella obeyed, but wept, because she too would have liked to go with them to the dance, and begged her step-mother to allow her to do so. "Thou go, Cinderella!" said she; "Thou art dusty and dirty and wouldst go to the festival? Thou hast no clothes and shoes, and yet wouldst dance!"'
-]],#4
+["Und als gerade ein junger Frischling dahergesprungen kam, stach er ihn ab, nahm Lunge und Leber heraus und brachte sie als Wahrzeichen der Königin mit. Der Koch mußte sie in Salz kochen, und das boshafte Weib aß sie auf und meinte, sie hätte Schneewittchens Lunge und Leber gegessen. ",
+"Nun war das arme Kind in dem großen Wald mutterseelenallein, und ward ihm so angst, daß es alle Blätter an den Bäumen ansah und nicht wußte, wie es sich helfen sollte. ",
+"Da fing es an zu laufen und lief über die spitzen Steine und durch die Dornen, und die wilden Tiere sprangen an ihm vorbei, aber sie taten ihm nichts. Es lief, so lange nur die Füße noch fortkonnten, bis es bald Abend werden wollte. ",
+"Da sah es ein kleines Häuschen und ging hinein, sich zu ruhen. In dem Häuschen war alles klein, aber so zierlich und reinlich, daß es nicht zu sagen ist. Da stand ein weißgedecktes Tischlein mit sieben kleinen Tellern, jedes Tellerlein mit seinem Löffelein, ferner sieben Messerlein und Gäblelein und sieben Becherlein. ",
+"An der Wand waren sieben Bettlein nebeneinander aufgestellt und schneeweiße Laken darüber gedeckt. Schneewittchen, weil es so hungrig und durstig war, aß von jedem Tellerlein ein wenig Gemüs' und Brot und trank aus jedem Becherlein einen Tropfen Wein; "]],#4
 [[1,1,1,1,1],
-['As, however, Cinderella went on asking, the stepmother at last said, "I have emptied a dish of lentils into the ashes for thee, if thou hast picked them out again in two hours, thou shalt go with us." ',
-'The maiden went through the back-door into the garden, and called, "You tame pigeons, you turtle-doves, and all you birds beneath the sky, come and help me to pick "The good into the pot, The bad into the crop." ',
-'Then two white pigeons came in by the kitchen-window, and afterwards the turtle-doves, and at last all the birds beneath the sky, came whirring and crowding in, and alighted amongst the ashes. ',
-'And the pigeons nodded with their heads and began pick, pick, pick, pick, and the rest began also pick, pick, pick, pick, and gathered all the good grains into the dish. Hardly had one hour passed before they had finished, and all flew out again. ',
-'Then the girl took the dish to her step-mother, and was glad, and believed that now she would be allowed to go with them to the festival. But the step-mother said, "No, Cinderella, thou hast no clothes and thou canst not dance; thou wouldst only be laughed at."'
+["denn es wollte nicht einem alles wegnehmen. Hernach, weil es so müde war, legte es sich in ein Bettchen, aber keins paßte; das eine war zu lang, das andere zu kurz, bis endlich das siebente recht war; und darin blieb es liegen, befahl sich Gott und schlief ein.",
+"Als es ganz dunkel geworden war, kamen die Herren von dem Häuslein, das waren die sieben Zwerge, die in den Bergen nach Erz hackten und gruben. Sie zündeten ihre sieben Lichtlein an, und wie es nun hell im Häuslein ward, sahen sie, daß jemand darin gesessen war, denn es stand nicht alles so in der Ordnung, wie sie es verlassen hatten.",
+"Der erste sprach: 'Wer hat auf meinem Stühlchen gesessen?' Der zweite: 'Wer hat von meinem Tellerchen gegessen?' Der dritte: »Wer hat von meinem Brötchen genommen?' ",
+"Der vierte: 'Wer hat von meinem Gemüschen gegessen?' Der fünfte: 'Wer hat mit meinem Gäbelchen gestochen?' Der sechste: 'Wer hat mit meinem Messerchen geschnitten?' Der siebente: 'Wer hat aus meinem Becherlein getrunken?' ",
+""
 ]],#5
 [[1,1,1,1,1],
 ['And as Cinderella wept at this, the step-mother said, "If thou canst pick two dishes of lentils out of the ashes for me in one hour, thou shalt go with us." And she thought to herself, "That she most certainly cannot do." ',
